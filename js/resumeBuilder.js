943ac1390833e0bbc640db7e9ca1062b7b8cea89
bijalba <bijalba@christophers-MacBook-Pro.local>
2015-05-04 09:40:27 -0500

    append / prepend and data swap
    prepend adds to beginning, append to end.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,18 @@
-//$("#main").append("Christopher Bijalba");
+/*$("#main").append("Christopher Bijalba");
 
 var awesomeThoughts = "I am Christopher Bijalba and I am AWESOME!"
 console.log(awesomeThoughts);
 
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
-$("#main").append(funThoughts);
+$("#main").append(funThoughts);*/
+
+var name = "Christopher Bijalba";
+var role = "Typographer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
