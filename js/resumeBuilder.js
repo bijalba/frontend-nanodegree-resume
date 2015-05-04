a6fe34b160d9054d85275387eb1321eae4bbf3f3
bijalba <bijalba@christophers-MacBook-Pro.local>
2015-05-04 09:31:55 -0500

    append and replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
-$("#main").append("Christopher Bijalba");
+//$("#main").append("Christopher Bijalba");
 
 var awesomeThoughts = "I am Christopher Bijalba and I am AWESOME!"
 console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
